de24ba70664ce184861a0e61f5e437fcd6154fea
Dairui Liu <rxhjxja@icloud.com>
2020-12-16 17:14:38 +0000

    update

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import os
+
 import pytorch_lightning as pl
 import yaml
 from pytorch_lightning import loggers as pl_loggers
@@ -22,21 +24,15 @@ if __name__ == "__main__":
     for i, (train, test) in enumerate(kfold.split(x_data, y_data)):
         train_loader = DataLoader(BiasNews(x_data[train], y_data[train]), batch_size=32)
         test_loader = DataLoader(BiasNews(x_data[test], y_data[test]), batch_size=1000)
+        model_dir = f"checkpoint/{model_name}/{i}/{config['head_num']}"
+        if not os.path.exists(model_dir):
+            break
+        model_path = os.path.join(model_dir, os.listdir(model_dir)[0])
         # sk-learn provides 10-fold CV wrapper.
         # init model
-        model = TitleBodyHeadAtt(hparam=hparams)
-        checkpoint_callback = ModelCheckpoint(
-            monitor="valid_acc",
-            dirpath=f"checkpoint/{model_name}/{i}/{config['head_num']}",
-            filename="{valid_acc:.3f}",
-            save_top_k=1,
-            mode="max",
-        )
         # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
-        trainer = pl.Trainer(gpus=1, min_epochs=20, max_epochs=30, check_val_every_n_epoch=1, logger=tb_logger,
-                             callbacks=[checkpoint_callback])
-        trainer.fit(model, train_loader, test_loader)
-        model = TitleBodyHeadAtt.load_from_checkpoint(checkpoint_callback.best_model_path, hparam=hparams)
+        trainer = pl.Trainer(gpus=-1)
+        model = TitleBodyHeadAtt.load_from_checkpoint(model_path, hparam=hparams)
         acc = trainer.test(model=model, test_dataloaders=test_loader)
         accuracy.append(acc[0]["test_acc"])
     print(f"Average test accuracy of {model_name}: {sum(accuracy) / len(accuracy)}")
