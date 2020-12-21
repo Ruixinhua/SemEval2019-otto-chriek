aa767003cb3684938a3fc367d1db10a4693978e8
Dairui Liu <rxhjxja@icloud.com>
2020-12-21 16:07:08 +0000

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo $HOME
+echo $PATH
 
+export PYENV_ROOT="/home/otto-chriek/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 pyenv activate bias-env
 python predict.py -A $1 -O $3/test.preds.txt
